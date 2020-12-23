43d90bc81170b40b8e3a44e1e288d947e0e1b93b
mutalisk <0x08@0x08.net>
2020-12-23 10:55:22 +0800

    update

--- util/ae64_test.go
+++ util/ae64_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestAe64Encode(t *testing.T) {
-	src := []byte("yambfpk3cat4eA7PAXD1a8dqEZzGkiDrZ5")
+	src := []byte("Qaa1jLayYg86BgxLDJrpqM9a7yhNUBvNkR")
 	src2 := []byte("12345678")
 	key := []byte("12345678")
 	dst, _ := Ae64Encode(src, key)
@@ -16,7 +16,7 @@ func TestAe64Encode(t *testing.T) {
 }
 
 func TestAe64Decode(t *testing.T) {
-	src := "X/anuih86ocszNESCwJw+KCDe1wce+1Dk7Q4zCBE9a/lnCwydhF7gSkmHvcdwPjk"
+	src := "Phgn95Se61hGId5ad70dNt9HFUhpZnFrzjMZLpPefcJq2MOPymyWRkXGsoiqQodt"
 	src2 := "m0lxCSrfYVhmOhZcOhICrw=="
 	key := []byte("12345678")
 	orgi, _ := Ae64Decode(src, key)
